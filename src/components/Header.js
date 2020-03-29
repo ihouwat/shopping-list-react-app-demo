4a936fa23370af7c7240c84be5b2c1f676bd9063
ihouwat <houwat@gmail.com>
2020-03-29 19:34:30 -0400

    Added menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
-import { Paper, Container, SvgIcon, IconButton, Box } from '@material-ui/core';
+import { Paper, Container, Box } from '@material-ui/core';
+import PopoverMenu from './PopoverMenu';
 
 const Header = () => {
   return (
@@ -18,15 +19,7 @@ const Header = () => {
             <Box fontSize={20}>
               <h1>My Shopping List</h1>
             </Box>
-            <IconButton 
-              disableFocusRipple
-              size='medium'
-              color='primary'
-            >
-              <SvgIcon  >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19 21" fill="black" width="19px" height="19px"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/></svg>
-              </SvgIcon>
-            </IconButton>
+            <PopoverMenu/>
           </Box>
         </Container>
       </Paper>
