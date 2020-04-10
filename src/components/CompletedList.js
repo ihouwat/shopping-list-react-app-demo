d9f72c4079d24a4ac89d83930cdf9309434b43bb
ihouwat <houwat@gmail.com>
2020-04-09 20:56:56 -0400

    fixed font colors on SVGs

--- src/components/CompletedList.js
+++ src/components/CompletedList.js
@@ -73,7 +73,11 @@ const CompletedList = ({ completedItems, deleteItem, recoverItem }) => {
     <div className={classes.root}>
       <ExpansionPanel className={classes.panel} expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
         <ExpansionPanelSummary
-          expandIcon={<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/><path d="M0 0h24v24H0z" fill="none"/></svg>}
+          expandIcon={
+            <SvgIcon>
+              <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+            </SvgIcon>
+          }
           aria-controls="panel1bh-content"
           id="panel1bh-header"
           className={classes.summary}
