402519f587286ca4bb80be1e8696efa3c857c9ab
ihouwat <houwat@gmail.com>
2020-05-17 15:58:29 -0400

    Updates

---
+++ precache-manifest.3e7734f42a24cb329582d8f7eb263d9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05cb327c05e7e8ffd300c3c40be1d66c",
+    "url": "/shopping-list-react-app-demo/index.html"
+  },
+  {
+    "revision": "6992af8667ad24d03810",
+    "url": "/shopping-list-react-app-demo/static/css/main.dccaaad9.chunk.css"
+  },
+  {
+    "revision": "a05c7a35148592e32d13",
+    "url": "/shopping-list-react-app-demo/static/js/2.8b41b365.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/shopping-list-react-app-demo/static/js/2.8b41b365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6992af8667ad24d03810",
+    "url": "/shopping-list-react-app-demo/static/js/main.9c7e0ced.chunk.js"
+  },
+  {
+    "revision": "12dc5a626691261d8cbd",
+    "url": "/shopping-list-react-app-demo/static/js/runtime-main.505e4d78.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shopping-list-react-app-demo/precache-manifest.b90b646155c5c47ff174ed593f304819.js"
+  "/shopping-list-react-app-demo/precache-manifest.3e7734f42a24cb329582d8f7eb263d9a.js"
 );
 
 self.addEventListener('message', (event) => {
